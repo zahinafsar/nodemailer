6a8df42f350c37c06db8ea7b58da255b38919859
Zahin Afsar <43959210+Mdzahin@users.noreply.github.com>
2020-02-17 05:33:23 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const exp = require('express');
+const app = exp();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+app.get('/:text', (req,res) => {
+
+    let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+let mailOptions = {
+    from: 'afsarzahin2@gmail.com',
+    to: 'afsarzahin@gmail.com',
+    subject: 'Message from zahin nodemailer',
+    text: req.params.text
+};
+
+transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+        res.send("failed");
+    }
+    res.send("Email done!!!");
+});
+})
+
+
+const port=process.env.PORT || 3000;
+app.listen(port);
