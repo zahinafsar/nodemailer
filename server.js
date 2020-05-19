a7c04f29b983b05cf7c94267683fec2100383a88
Zahin Afsar <43959210+Mdzahin@users.noreply.github.com>
2020-05-19 22:04:33 +0600

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ let mailOptions = {
     from: 'afsarzahin2@gmail.com',
     to: 'afsarzahin@gmail.com',
     subject: 'Message from zahin nodemailer',
-    text: req.params.text
-};
+    text: {
+           from: req.params.email,
+           message: req.params.text
+          };
 
 transporter.sendMail(mailOptions, (err, data) => {
     if (err) {
